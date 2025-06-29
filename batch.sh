bb4103d5ce8f7128da457758bd6181c0d7dcf6ae
DX Lim <dxlim@outlook.com>
2025-06-29 14:55:44 -0700

    automatically process all files in recordings

--- batch.sh
+++ batch.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #bash always does stuff sequentially hopefully
-for i in {1..3}; do
-  echo "processing record$i"
-  cp recordings/record$i recordings/target
+#for i in {1..3}; do
+rm recordings/target
+for i in $(ls recordings); do
+  echo "processing $i."
+  cp recordings/$i recordings/target
   echo "file copied, starting python script."
   python ppo.py
   echo "script finished."
