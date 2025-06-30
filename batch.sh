e4b0739c9b120cde03b8dde5829e1c3665c123fd
DX Lim <dxlim@outlook.com>
2025-06-29 23:46:46 -0700

    edited the script to pass the game parameter correctly

--- batch.sh
+++ batch.sh
@@ -2,6 +2,7 @@
 #bash always does stuff sequentially hopefully
 #for i in {1..3}; do
 game="Tetris-GameBoy"
+game="Pong-Atari2600"
 path="recordings/$game"
 echo $path
 rm recordings/target
@@ -9,6 +10,6 @@ for i in $(ls $path); do
   echo "processing $path/$i."
   cp $path/$i recordings/target
   echo "file copied, starting python script."
-  python ppo.py
+  python ppo.py --game $game
   echo "script finished."
 done
