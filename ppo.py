c423fde7212cd4279c267e1258860fb1c18daee4
DX Lim <dxlim@outlook.com>
2025-06-30 20:18:18 -0700

    doesn't really make sense to have an inner loop to collect rewards.  removed inner loop.

--- ppo.py
+++ ppo.py
@@ -19,18 +19,15 @@ import pickle
 RENDERMODE='human'
 RENDERMODE=None
 
-CONTROLLERSTEPS=4           #original value
 MODELTOTALTIMESTEPS=2048    #2048 seems to be the minimum value
 
-CONTROLLERSTEPS=4
 MODELTOTALTIMESTEPS=2048*32
 
-#CONTROLLERSTEPS=4*8
 #MODELTOTALTIMESTEPS=2048*32/8
 
 class TetrisController(gym.Wrapper):
 
-    def __init__(self, env, n, recording):
+    def __init__(self, env, recording):
         self.ball_x0=-1
         self.returnedBall=False
         self.prevError=0
@@ -42,7 +39,6 @@ class TetrisController(gym.Wrapper):
         self.records=[]
 
         gym.Wrapper.__init__(self, env)
-        self.n = n
         self.curac = None
 
     def reset(self, **kwargs):
@@ -56,58 +52,55 @@ class TetrisController(gym.Wrapper):
         totrew = 0
         prevoffset=0
         offset=0
-        for i in range(self.n):
-            #self.curac = ac
-            #print(self.curac)
+        #self.curac = ac
+        #print(self.curac)
 
 
-            #offset=random.randint(0,8)
+        #offset=random.randint(0,8)
 
-            offset=random.randint(0,4)
-            if(offset>0):
-                offset+=4
+        offset=random.randint(0,4)
+        if(offset>0):
+            offset+=4
 
-            #print(offset)
+        #print(offset)
 
-            self.curac=[0]*9
-            self.curac[offset]=1
-            
-            #import time
-            #time.sleep(0.2)
-            recordlen=len(self.replayRecords)
-            if(recordlen>0):
-                self.curac=self.replayRecords.pop()
-                self.curac=list(map(int,self.curac))
-                #print(recordlen,self.curac)
-            else:
-                assert(False)
+        self.curac=[0]*9
+        self.curac[offset]=1
+        
+        #import time
+        #time.sleep(0.2)
+        recordlen=len(self.replayRecords)
+        if(recordlen>0):
+            self.curac=self.replayRecords.pop()
+            self.curac=list(map(int,self.curac))
+            #print(recordlen,self.curac)
+        else:
+            assert(False)
 
-            ob, rew, terminated, truncated, info = self.env.step(self.curac)
+        ob, rew, terminated, truncated, info = self.env.step(self.curac)
 
-            #print(info)
-            if False:
-                if(info['ball_x'] < self.ball_x0):
-                    if(not self.returnedBall):
-                        #print(i,'returned ball.')
-                        self.returnedBall = True
-                        rew = 1
-                else:
-                    self.returnedBall = False
-                self.ball_x0=info['ball_x']
-            if False:
-                error=abs(info['ball_y']-info['p1_pos'])
-                if(error>self.prevError):
-                    rew = -1
-                else:
+        #print(info)
+        if False:
+            if(info['ball_x'] < self.ball_x0):
+                if(not self.returnedBall):
+                    #print(i,'returned ball.')
+                    self.returnedBall = True
                     rew = 1
-                self.prevError=error
-                #print(rew)
+            else:
+                self.returnedBall = False
+            self.ball_x0=info['ball_x']
+        if False:
+            error=abs(info['ball_y']-info['p1_pos'])
+            if(error>self.prevError):
+                rew = -1
+            else:
+                rew = 1
+            self.prevError=error
+            #print(rew)
 
-            #print(self.curac)
-            totrew += rew
-            #print('reward: ',rew,'/',totrew)
-            if terminated or truncated:
-                break
+        #print(self.curac)
+        totrew += rew
+        #print('reward: ',rew,'/',totrew)
         return ob, totrew, terminated, truncated, info
 
 
@@ -132,13 +125,13 @@ def ppoMain():
         replayRecords=pickle.load(f)
         nrecords=len(replayRecords)
         print('nrecords=',nrecords)
-        MODELTOTALTIMESTEPS=int(math.floor(nrecords/2048/4)*2048)
+        MODELTOTALTIMESTEPS=int(math.floor(nrecords/2048)*2048)
         print(MODELTOTALTIMESTEPS)
 
     def make_env():
         recording='recordings/target'
         env = retro.make(args.game, args.state, scenario=args.scenario, render_mode=RENDERMODE)
-        env = TetrisController(env, CONTROLLERSTEPS, recording)
+        env = TetrisController(env, recording)
         env.reset(seed=0)
         env = wrap_deepmind_retro(env)
         return env
